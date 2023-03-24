d9df6126380f4fcfc35512362d20f4394d17274e
Lokdir <laurie.sebban@e-artsup.net>
2023-03-24 15:15:01 +0100

    (PLAYER) Détection du clic selon le tag de l'objet

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class Player : Character
 {
-    Ennemy target;
+    RaycastHit lastTarget;
+    Ray rayFromCameraPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rayFromCameraPosition = Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) //OnClick
+        {
+            if (Physics.Raycast(rayFromCameraPosition, out lastTarget)) //Ca touche quelque chose
+            {
+                if (lastTarget.collider.gameObject.tag == "Ennemy") //Ca touche un ennemi
+                {
+                     //Si inRange
+                     //  Attaquer
+                     //Sinon
+                     //  Move
+                }
+                else //Ca touche autre chose
+                {
+                     //Move
+                }
+            }
+        }
     }
 
-    void Targetting() { }
+    void Targetting() 
+    {
+
+    }
+
 }
